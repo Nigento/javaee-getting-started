4a1512f758f7c9ac6c9ac33f00b41d54631bb287
Nigento <nigento.ningen@outlook.fr>
2021-03-04 17:11:52 +0100

    v5
    The book entity is created and the application is deploying to the server

---
+++ bookstore-back/src/main/java/com/pluralsight/bookstore/model/Book.java
@@ -0,0 +1,112 @@
+package com.pluralsight.bookstore.model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Book {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(length = 200)
+    private String title;
+    @Column(length = 1000)
+    private String description;
+    @Column(name ="unit_cost")
+    private Float unitCost;
+
+    public String isbn;
+
+    @Column(name ="publication_date")
+    @Temporal(TemporalType.DATE)
+    private Date publicationDate;
+
+    @Column(name="number of pages")
+    private Integer nbOfPages;
+
+    @Column(name="image_url")
+    private String imageUrl;
+
+    private Language language;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(Float unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public Date getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(Date publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public Integer getNbOfPages() {
+        return nbOfPages;
+    }
+
+    public void setNbOfPages(Integer nbOfPages) {
+        this.nbOfPages = nbOfPages;
+    }
+
+    public Language getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(Language language) {
+        this.language = language;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", unitCost=" + unitCost +
+                ", publicationDate=" + publicationDate +
+                ", nbOfPages=" + nbOfPages +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", language=" + language +
+                '}';
+    }
+}
